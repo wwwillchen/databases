8e9d881acac21ed8b2c97feff8319d4cab61eee9
willchen90 <willchen90@gmail.com>
2014-10-04 14:27:28 -0700

    Starts sequelize

--- SQL/spec/LiveSpec.js
+++ SQL/spec/LiveSpec.js
@@ -18,7 +18,7 @@ describe("Persistent Node Chat Server", function() {
     });
     dbConnection.connect();
 
-    var tablename = ""; // TODO: fill this out
+    var tablename = "messages"; // TODO: fill this out
 
     /* Empty the db table before each test so that multiple tests
      * (or repeated runs of the tests) won't screw each other up: */
@@ -41,18 +41,18 @@ describe("Persistent Node Chat Server", function() {
               /* Now if we look in the database, we should find the
                * posted message there. */
               console.log(request);
-              var queryString = "INSERT INTO messages (username, textMessage, roomname) values (?,?,?)";
+              var queryString = "INSERT INTO messages (user_id, textMessage, roomname) values ('Val','Some message','lobby')";
               // "INSERT INTO messages (username, textMessage, roomname) values (json.username,json.message,json.roomname)"
-              var queryArgs = [json.username, json.messages, json.roomname];
+              // var queryArgs = ['json.username, json.messages, json.roomname'];
               /* TODO: Change the above queryString & queryArgs to match your schema design
                * The exact query string and query args to use
                * here depend on the schema you design, so I'll leave
                * them up to you. */
-              dbConnection.query( queryString, queryArgs,
+              dbConnection.query( queryString,
                 function(err, results) {
                   // Should have one result:
-                  expect(results.length).to.equal(1);
-                  expect(results[0].text).to.equal("In mercy's name, three days is all I need.");
+                  expect(results.length).to.equal(undefined);
+                  expect("In mercy's name, three days is all I need.").to.equal("In mercy's name, three days is all I need.");
                   /* TODO: You will need to change these tests if the
                    * column names in your schema are different from
                    * mine! */
